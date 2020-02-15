df4b847f54de12477842ff6a553dbad377fc2362
Igor Doric <doricigor@yahoo.com>
2020-02-15 22:23:20 +0100

    Done for today

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+    // Tabs
+    $('.tabsList__tabs a').on('click', function (event) {
+        event.preventDefault();
+        const num = $(this).attr('href');
+        console.log(num);
+        $('.tab-active').removeClass('tab-active');
+        $(this).addClass('tab-active');
+        $(num).slideDown().siblings().slideUp();
+    });
+
+
+    // Slick slider for Categories 
+
+    $('.sliderWrap').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        sliderWrap: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
+});

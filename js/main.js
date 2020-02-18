e643f2ccfd57012db3c7f9ec5cd43f0928867b30
doricigor <doricigor@yahoo.com>
2020-02-18 17:06:19 +0100

    Fhinish for today in office

--- js/main.js
+++ js/main.js
@@ -16,12 +16,102 @@ $(document).ready(function() {
     $('.slider__wrap').slick({
         dots: true,
         arrows: true,
-        // autoplay: true,
+        autoplay: true,
         autoplaySpeed: 2000,
         infinite: false,
         speed: 300,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
+
+      // Slick slider for products
+      
+      $('.spotlight__wrap').slick({
+        dots: true,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
+      // Slick slider for reviews
+
+      $('.reviews__wrap').slick({
+        dots: true,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        responsive: [
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
+      // Slick slider for clients
+
+      $('.clients__wrap').slick({
+        dots: true,
+        arrows: true,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 5,
         responsive: [
           {
             breakpoint: 1024,
